b6a9e6cc0a30c996906e5d0ac9a8ed92bac704ce
maximilien lehujeur <maximilien.lehujeur@gmail.com>
2020-11-07 13:43:51 +0100

    start dbtools repo

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# DBTOOLS
+
+Install
+```
+# cd <to/installation/path> 
+git clone https://gitlab.com/obsmax/dbtools.git
+
+conda remove --name py37-dbtools --all --yes
+conda create --name py37-dbtools python=3.7 --yes
+conda activate py37-dbtools
+
+cd dbtools
+python -m pip install -e .
+
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import os
+
+# fuck distutils2
+version_file = os.path.join('dbtools', 'version.py')
+if not os.path.isfile(version_file):
+    raise IOError(version_file)
+
+with open(version_file, "r") as fid:
+    for l in fid:
+        if l.strip('\n').strip().startswith('__version__'):
+            __version__ = l.strip('\n').split('=')[-1].split()[0].strip()
+            break
+    else:
+        raise Exception(f'could not detect __version__ affectation in {version_file}')
+
+
+with open("Readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dbtools", # Replace with your own username
+    version=__version__,
+    author="Maximilien Lehujeur",
+    author_email="maximilien.lehujeur@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy'],
+    scripts=[""])
